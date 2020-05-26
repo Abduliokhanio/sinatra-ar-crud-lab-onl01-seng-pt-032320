8c68c1a1e8ac530eb82abb52e83b8bf1c10ae0ea
Abdul Khan <akhan17@outlook.com>
2020-05-26 21:03:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,13 @@ class ApplicationController < Sinatra::Base
       erb :edit
   end
 
+  patch "/articles/:id" do
+      @article = Article.find(params[:id])
+      # @article.update(title: params[:title], content: params[:content])
+      @article.update(params[:article])
+      redirect to "/articles/#{ @article.id }"
+    end
+
 
 
 end
