272426d8b32fdefe2e01abf3fd2e1e4c3a991fa4
Abdul Khan <akhan17@outlook.com>
2020-05-26 20:52:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-  Article.create(params)
-  redirect '/articles/'
+    @article = Article.create(title: params[:title], content: params[:content])
+        redirect to "/articles/#{@article.id}"
   end
 
 
